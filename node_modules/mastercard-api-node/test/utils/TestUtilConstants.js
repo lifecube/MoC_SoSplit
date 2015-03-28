4a7598f5d515e5d38f1a149bf32c6b4cab4e4b83
Tang Hao (Peter) <hao_tang@mastercard.com>
2015-03-28 14:23:55 +0800

    add mastercard-api-node

---
+++ node_modules/mastercard-api-node/test/utils/TestUtilConstants.js
@@ -0,0 +1,11 @@
+const SANDBOX_PRIVATE_KEY_PATH = 'C:\\Users\\kdw0609\\Documents\\privateKey.pem';
+const PRODUCTION_PRIVATE_KEY_PATH = 'C:\\Users\\tvc0725\\dev\\mastercard\\keystore\\production\\privateKey.pem';
+const PRIVATE_KEY_PASSWORD = 'P@$$W0rd';
+const SANDBOX_CONSUMER_KEY = 'LYInUwnKbFZrFDYbdhTFaC7rwTUuF-lBxnYQRYd21843a245!414e54324a6332497a356b4634396e65345244734661413d';
+const PRODUCTION_CONSUMER_KEY = 'tGyD_uBFZgC0cvN-I2erNTPknc8eWHzn8r2JQ3bk15ec3778!414c64374c4c47657033656356447a6553627a6f4a774d3d';
+
+module.exports.sandbox_private_key_path = SANDBOX_PRIVATE_KEY_PATH;
+module.exports.production_private_key_path = PRODUCTION_PRIVATE_KEY_PATH;
+module.exports.private_key_password = PRIVATE_KEY_PASSWORD;
+module.exports.sandbox_consumer_key = SANDBOX_CONSUMER_KEY;
+module.exports.production_consumer_key = PRODUCTION_CONSUMER_KEY;
